18fc538ebc500d79f6bddabf7314a4cbfbafed8b
R. Kevin Nelson <kevin@rkn.la>
2013-04-29 21:48:13 -0700

    Initial Commit

---
+++ GeoExperiment/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GeoExperiment
+//
+//  Created by Kevin Nelson on 4/29/13.
+//  Copyright (c) 2013 rknLA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GeoExperiment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GeoExperiment
+//
+//  Created by Kevin Nelson on 4/29/13.
+//  Copyright (c) 2013 rknLA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
